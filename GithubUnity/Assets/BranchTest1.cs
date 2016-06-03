812adf7b9889295eef7028d2cc75b3b20c87d389
sorhe <sorhexg@outlook.com>
2016-06-03 16:08:42 +0800

    all works

---
+++ GithubUnity/Assets/BranchTest1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BranchTest1 : MonoBehaviour {
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(transform.forward, Space.World);
+        }
+    }
+}
